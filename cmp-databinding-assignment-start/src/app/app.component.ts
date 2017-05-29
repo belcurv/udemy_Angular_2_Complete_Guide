550757971930259e222b8ef492686720eb29f30d
Jay Schwane <jrschwane@uwalumni.com>
2017-05-29 15:55:26 -0500

    Finish databinding assignment

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public odds: number[] = [];
+  public evens: number[] = [];
+
+  public onCountChange(count: number) {
+
+    console.log(count);
+
+    if (count % 2 === 0) {
+      this.evens.push(count);
+    } else {
+      this.odds.push(count);
+    }
+  }
+
 }
--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -10,13 +10,22 @@ export class GameControlComponent implements OnInit {
   public gameRunning: boolean = false;
   public count: number = 0;
 
+  // define and output our interval event emitter
+  @Output() public intervalEvent = new EventEmitter<number>();
+
   private _setIntervalHandler: any;
 
 
   onStartGame() {
     this.gameRunning = true;
     this._setIntervalHandler = setInterval(() => {
+
+      // increment count
       this.count += 1;
+
+      // emit our event
+      this.intervalEvent.emit(this.count);
+
     }, 1000);
   }
 
